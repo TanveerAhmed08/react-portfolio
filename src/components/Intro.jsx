822621c4f3b0e23c01b072e0daf619122f46e62d
Tanveer Ahmed <tanveerahmed08.1999@gmail.com>
2024-08-22 01:00:14 +0500

    React-work

---
+++ src/components/Intro.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import Logo from "../assets/boy.png";
+import { delay, motion } from "framer-motion";
+
+const container = (delay) => ({
+  hidden: { x: -100, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+
+    transition: { duration: 0.5, delay: delay },
+  },
+});
+const container2 = (delay) => ({
+  hidden: { x: 100, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+
+    transition: { duration: 0.5, delay: delay },
+  },
+});
+
+const Intro = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-4 lg:mb-35">
+      <div className="flex flex-wrap">
+        <div className="w-full lg:w-1/2">
+          <div className="flex flex-col items-center lg:items-start">
+            <motion.h1
+              variants={container(0)}
+              initial="hidden"
+              animate="visible"
+              className="pb-7 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl"
+            >
+              Tanveer Ahmed
+            </motion.h1>
+            <motion.span
+              variants={container(0.5)}
+              initial="hidden"
+              animate="visible"
+              className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent"
+            >
+              Front-End Developer
+            </motion.span>
+            <motion.p
+              variants={container(1)}
+              initial="hidden"
+              animate="visible"
+              className="my-2 max-w-xl py-6 font-light tracking-tighter"
+            >
+              I'm a seasoned front-end developer with fresh experience create
+              website using programming languages such as HTML, CSS, JavaScript.
+              <br />
+              And using libraries such as BootStrap, tailwind.
+            </motion.p>
+          </div>
+        </div>
+        <div className="w-full lg:w-1/2 lg:p-8">
+          <div className="flex justify-center">
+            <motion.img 
+             variants={container2(1.5)}
+             initial="hidden"
+             animate="visible"
+            src={Logo} alt="" height={300} width={300} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
---
+++ src/constants/index.js
@@ -0,0 +1,58 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const EXPERIENCES = [
+  {
+    year: "2023 - Present",
+    role: "Information-Technology Assistant Manager",
+    company: "Chottani Industries",
+    description: `Clint-side Troubleshooting, Server-side Installation, Camera Installation, Microsoft Active Directory with basic knowladge such as Group Policy | User Management `,
+    technologies: ["Server 2016", "Webmail", "Kasper"],
+  },
+  {
+    year: "2018 - 2022",
+    role: "Former-Salesman",
+    company: "Khatri Smart Tech",
+    description: `All types of Software & Hardware Troubleshooting, Windows & Software installation, Sales Person & Marketing`,
+    technologies: ["Windows7, 8.1, 10", "System Assembling"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "TO-DO List",
+    image: project1,
+    description:
+      "User can type all the task that he doing in his daily routine and after done he delete the task.",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+  {
+    title: "Weather",
+    image: project2,
+    description:
+      "User Can see the weather of all Cities & Countries",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+  {
+    title: "Wildlife",
+    image: project3,
+    description:
+      "All Information About Wildlife usre can read information about animals.",
+    technologies: ["HTML", "CSS", "Bootstrap"],
+  },
+  {
+    title: "Pizza Shop",
+    image: project4,
+    description:
+      "User Can Buy A pizza from online method and see all price of pizza's in the website ",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+];
+
+export const CONTACT = {
+  address: "PLOT NO 1737 ATT BAGHDADI LYARI QUARTERS LYARI FLAT NO 501",
+  phoneNo: "+923053565751",
+  email: "tanveerahmed08.1999@gmail.com",
+};
